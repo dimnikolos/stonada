36962e6d4dd676a7d04195679a576154efdbca2f
Dimitris Nikolos <dnikolos@gmail.com>
2026-01-07 23:06:15 +0200

    mutation observer

--- firefox/content.js
+++ firefox/content.js
@@ -1,39 +1,42 @@
 /*DIAVGEIA-------------------------------------------------*/
 
-function hasEnglish(s){
-  for (let i=0; i<s.length; i++){
-    code = s[i].charCodeAt(0);
-    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)){
-      return true;
-    }
-  }
-  return false;
+function validADA(s) {
+  return /^[Α-Ωα-ω0-9\-]+$/.test(s);
 }
 
-  const waitForElement = setInterval(() => {
-    const input = document.getElementById('generalSearchText');
+function createButton() {
+  const input = document.getElementById("generalSearchText");
+  const container = input.parentElement;
 
-    if (input) {
-      clearInterval(waitForElement); // stop polling once found
+  const newButton = document.createElement("button");
+  newButton.textContent = "Κατευθείαν στον ΑΔΑ!";
+  newButton.className = "btn s-btn";
+  newButton.type = "button";
+  newButton.title = "Κατευθείαν στον ΑΔΑ!";
+  newButton.style.marginLeft = "5px";
 
-      const container = input.parentElement;
-
-      const newButton = document.createElement('button');
-      newButton.textContent = 'Κατευθείαν στον ΑΔΑ!';
-      newButton.className = 'btn s-btn';
-      newButton.type = 'button';
-      newButton.title = 'Κατευθείαν στον ΑΔΑ!';
-      newButton.style.marginLeft = '5px';
-
-      newButton.addEventListener('click', () => {
-        if (hasEnglish(document.getElementById("generalSearchText").value)){
-          alert('Ο ΑΔΑ δεν μπορεί να έχει αγγλικούς χαρακτήρες!');
-        }
-        else{
-          window.location.href = "https://diavgeia.gov.gr/decision/view/" + document.getElementById("generalSearchText").value.toUpperCase();
-        }
-      });
-
-      container.appendChild(newButton);
+  newButton.addEventListener("click", () => {
+    if (!validADA(document.getElementById("generalSearchText").value)) {
+      alert("Δεν είναι ΑΔΑ!");
+    } else {
+      window.location.href =
+        "https://diavgeia.gov.gr/decision/view/" +
+        document.getElementById("generalSearchText").value.toUpperCase();
     }
-  }, 300); // check every 300ms
+  });
+
+  container.appendChild(newButton);
+}
+
+const observer = new MutationObserver((mutations, obs) => {
+  const targetElement = document.querySelector("#generalSearchText");
+  if (targetElement) {
+    createButton();
+    obs.disconnect(); // Σταμάτα να παρακολουθείς
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
